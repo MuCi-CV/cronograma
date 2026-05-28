846daf2c8ae791b9072603f041e0f3ae72871578
Carlos Vallory <carlvallory@gmail.com>
2026-05-28 12:07:53 -0300

    feat: eager-load images and pass layout settings to views

--- app/Http/Controllers/TrackerController.php
+++ app/Http/Controllers/TrackerController.php
@@ -2,25 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Setting;
 use App\Models\Stage;
 
 class TrackerController extends Controller
 {
     public function visual()
     {
-        $stages = Stage::with(['sections.items.latestUpdate'])
+        $stages = Stage::with(['sections.items.latestUpdate', 'sections.items.images'])
             ->orderBy('order')
             ->get();
 
-        return view('tracker.visual', compact('stages'));
+        $imageLayout      = Setting::get('image_layout', 'B');
+        $hideImagesMobile = Setting::get('hide_images_mobile', '0') === '1';
+
+        return view('tracker.visual', compact('stages', 'imageLayout', 'hideImagesMobile'));
     }
 
     public function detail()
     {
-        $stages = Stage::with(['sections.items.latestUpdate'])
+        $stages = Stage::with(['sections.items.latestUpdate', 'sections.items.images'])
             ->orderBy('order')
             ->get();
 
-        return view('tracker.detail', compact('stages'));
+        $imageLayout      = Setting::get('image_layout', 'B');
+        $hideImagesMobile = Setting::get('hide_images_mobile', '0') === '1';
+
+        return view('tracker.detail', compact('stages', 'imageLayout', 'hideImagesMobile'));
     }
 }
